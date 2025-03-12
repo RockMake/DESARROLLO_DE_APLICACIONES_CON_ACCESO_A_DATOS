5995f57fd944c9015dfff52179c4206e7f465320
Duban Leonardo ramirez Vasquez <54449565+RockMake@users.noreply.github.com>
2025-03-12 08:26:44 -0500

    Add files via upload
    Entrega de primera tarea

---
+++ tienda.py
@@ -0,0 +1,67 @@
+# Ejercicio 2: Sistema de Tienda Online
+# Objetivo: Crear una estructura para una tienda online con productos, pedidos y clientes.
+
+# Requerimientos:
+# La tienda debe tener:
+
+# Lista de productos (id, nombre, precio y stock).
+
+# Lista de pedidos (id_pedido, cliente, productos solicitados y estado).
+
+# Clientes registrados (nombre, email y dirección).
+
+# 📌 Reto extra:
+
+# Agrega un nuevo producto (ej: "Pantalón", $39.99, stock 75).
+
+# Cambia el estado del pedido 101 a "Entregado".
+
+# Encuentra todos los pedidos de un cliente específico por email.
+
+productos = [
+    {"id": 1, "nombre": "Camiseta", "precio": 19.99, "stock": 50},
+    {"id": 2, "nombre": "Zapatos", "precio": 59.99, "stock": 20},
+    {"id": 3, "nombre": "Gorra", "precio": 19.99, "stock": 20}
+]
+clientes = [
+    {"nombre": "Sasuke", "correo": "sasuke@correo", "direccion": "Calle 33 KRA 54"},
+    {"nombre": "Naruto", "correo": "naruto@correo", "direccion": "KRA 48 CL 52"},
+    {"nombre": "Shakira", "correo": "shakira@correo", "direccion": "KRA 20 CL 52"}
+]
+pedidos = [
+    {"id_pedido": 101, "cliente":clientes[0], "productos":[(productos[0]), (productos[1])]},
+    {"id_pedido": 102, "cliente":clientes[1], "productos":[(productos[1]), (productos[2])]},
+    {"id_pedido": 103, "cliente":clientes[2], "productos":[(productos[2]), (productos[0])]}
+]
+
+
+def menu():
+    while True:
+        print("\nMenu de opciones")
+        print("1 Verificar productos")
+        print("2 ver clientes")
+        print("3 ver pedidos")
+        print("4 Agregar un producto")
+        print("5 Confirmar estado de pedido")
+        print("6 Buscar pedido por correo")
+        
+
+        opcion = int(input("Elija una opción: "))
+
+        match opcion:
+            case 1:
+                print("\nLista de productos:")
+                for producto in productos:
+                    print(f"ID: {producto['id']}, Nombre: {producto['nombre']}, Precio: ${producto['precio']}, Stock: {producto['stock']}")
+            case 2:
+                print("\nLisa de clientes")
+                for cliente in clientes:
+                    print(f"Nombre: {cliente['nombre']}, Correo: {cliente['correo']}, direccion: {cliente['direccion']}")
+            case 3:
+                print("\nLista de pedidos:")
+                for pedido in pedidos:
+                    print(f"ID_pedido: {pedido['id_pedido']}, Cliente: {pedido['cliente']['nombre']}, Estado: {pedido.get('estado', 'Pendiente')}")
+                    print("Productos:")
+                    for prod in pedido["productos"]:
+                        print(f"   - {prod['nombre']}")
+menu()
